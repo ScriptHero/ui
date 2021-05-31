78f0182ec3795049e3e9e40def2eb883efd546df
Jim Muir <jmuir@covermymeds.com>
2021-05-31 18:30:21 -0400

    WrapNode helper

---
+++ src/components/wrapper/WrapNode.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+
+import Box from '../box/Box.vue'
+
+export default defineComponent({
+  props: {
+    value: {type: [Object, Array], required: true},
+    tag: {type: String, default: 'div'},
+    options: {
+      type: Object, default: () => {
+      }
+    },
+  },
+  render() {
+    let nodes = this.value
+    if (!Array.isArray(nodes)) {
+      nodes = [nodes]
+    }
+
+    return h(Box, this.options, nodes)
+  }
+})
+</script>
