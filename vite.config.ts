6e34acd0b570134d77787f47113d1168284340e7
Jim Muir <jmuir@covermymeds.com>
2021-06-17 13:18:13 -0400

    Try some more updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,20 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const resolve = pathSegments => path.resolve(__dirname, pathSegments)
-const prod = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   plugins: [
     vue(),
     vanillaExtractPlugin()
   ],
-  root: resolve('src'),
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@scripthero/ui',
       formats: ['es']
     },
-    outDir: resolve('dist'),
     emptyOutDir: true,
     rollupOptions: {
       external: ['vue'],
